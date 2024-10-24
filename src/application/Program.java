a11430ba7d961e2a4f3ffd1dc8042c7d24f31d2c
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-10-23 23:38:49 -0300

    Implementation using Predicate Interface.

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import utils.ProductPredicate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> products = new ArrayList<>();
+		
+		products.add(new Product("Macbook Pro", 25000.00));
+		products.add(new Product("iPhone 16", 16000.00));
+		products.add(new Product("Notebook Lenovo", 5000.00));
+		products.add(new Product("Airpods", 200.00));
+		products.add(new Product("Smart TV LG 4K", 4850.00));
+		products.add(new Product("External SSD 1Tb", 785.00));
+		
+		products.removeIf(new ProductPredicate());
+		
+		for(Product p : products) {
+			System.out.println(p);
+		}
+	}
+}
