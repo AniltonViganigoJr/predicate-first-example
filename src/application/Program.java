f93b2df3822c7949cf12c3e5b0935986c8e79c76
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-10-25 00:19:34 -0300

    Example using Declarated Lambda Expression.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -18,7 +19,11 @@ public class Program {
 		products.add(new Product("Smart TV LG 4K", 4850.00));
 		products.add(new Product("External SSD 1Tb", 785.00));
 		
-		products.removeIf(Product::nonStaticProductPredicate);
+		double min = 1000.00;
+		
+		Predicate<Product> productPredicate = (p -> p.getPrice() >= min);
+		
+		products.removeIf(productPredicate);
 		
 		for(Product p : products) {
 			System.out.println(p);
